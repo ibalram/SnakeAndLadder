5d5aba57293831bde5eaedde2d8fc9f16541cbc1
ibalram <balramsinghrathore3@gmail.com>
2020-09-25 03:21:45 +0530

    Added: Repeat till player reaches 100

--- src/com/cg/SnakeAndLadder.java
+++ src/com/cg/SnakeAndLadder.java
@@ -16,28 +16,34 @@ public class SnakeAndLadder {
 		int MAX_ROLL = 6;
 		Random rand = new Random();
 		
+		
 		for( int player = 1; player <= NO_OF_PLAYERS; ++player) {
-			System.out.println("Position of player "+ player + " is " + position[player-1]);
-			
-			int diceRoll = rand.nextInt(MAX_ROLL) +1;
-			System.out.println("The dice roll for player "+ player + " is " + diceRoll);
-			
-			int option = rand.nextInt(3)+1;
-			
-			switch(option) {
-			case 1:
-				System.out.println(String.format("No play, Player %d remains at the same position, which is %d", player,position[player-1]));
-				break;
-			case 2:
-				position[player-1] += diceRoll;
-				System.out.println(String.format("Wow!! it's Ladder, Player %d moves ahead by %d steps", player, diceRoll));
-				System.out.println(String.format("Updated position of the player %d is %d", player, position[player-1]));
-				break;
-			default:
-				position[player-1] -= diceRoll;
-				System.out.println(String.format("Oops.. it's Snake, Player %d moves behind by %d steps", player, diceRoll));
-				System.out.println(String.format("Updated position of the player %d is %d", player, position[player-1]));
+			while(position[player-1]<=100) {
+				System.out.println("Position of player "+ player + " is " + position[player-1]);
+				
+				int diceRoll = rand.nextInt(MAX_ROLL) +1;
+				System.out.println("The dice roll for player "+ player + " is " + diceRoll);
+				
+				int option = rand.nextInt(3)+1;
+				
+				switch(option) {
+				case 1:
+					System.out.println(String.format("No play, Player %d remains at the same position, which is %d", player,position[player-1]));
+					break;
+				case 2:
+					position[player-1] += diceRoll;
+					System.out.println(String.format("Wow!! it's Ladder, Player %d moves ahead by %d steps", player, diceRoll));
+					System.out.println(String.format("Updated position of the player %d is %d", player, position[player-1]));
+					break;
+				default:
+					position[player-1] -= diceRoll;
+					if (position[player-1] < 0 ) position[player-1] = 0;
+					System.out.println(String.format("Oops.. it's Snake, Player %d moves behind by %d steps", player, diceRoll));
+					System.out.println(String.format("Updated position of the player %d is %d", player, position[player-1]));
+				}
+				System.out.println();
 			}
+			System.out.println(String.format("Player %d reached 100", player));
 		}
 
 	}
