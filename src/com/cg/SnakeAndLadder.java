770ea5f2cf2b063e09bfdd930a59a7801a5b340a
ibalram <balramsinghrathore3@gmail.com>
2020-09-25 02:22:53 +0530

    Added: initialise for single player

--- src/com/cg/SnakeAndLadder.java
+++ src/com/cg/SnakeAndLadder.java
@@ -1,9 +1,18 @@
 package com.cg;
 
+import java.util.Arrays;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Snake and ladder game simulator");
+		
+		int NO_OF_PLAYERS = 1;
+		int position[] = new int[NO_OF_PLAYERS];
+		Arrays.fill(position, 0);
+		for( int i = 0; i< NO_OF_PLAYERS; ++i) {
+			System.out.println("Position of player "+ String.format("%d",i+1) + " is " + position[i]);
+		}
 
 	}
 
