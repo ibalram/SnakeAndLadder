5e518e4538f43e9575a4ab54586f47357afc9274
ibalram <balramsinghrathore3@gmail.com>
2020-09-25 03:09:20 +0530

    Added: Check for option

--- src/com/cg/SnakeAndLadder.java
+++ src/com/cg/SnakeAndLadder.java
@@ -7,6 +7,7 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Snake and ladder game simulator");
+		System.out.println("-------------------------------");
 		
 		int NO_OF_PLAYERS = 1;
 		int position[] = new int[NO_OF_PLAYERS];
@@ -21,6 +22,22 @@ public class SnakeAndLadder {
 			int diceRoll = rand.nextInt(MAX_ROLL) +1;
 			System.out.println("The dice roll for player "+ player + " is " + diceRoll);
 			
+			int option = rand.nextInt(3)+1;
+			
+			switch(option) {
+			case 1:
+				System.out.println(String.format("No play, Player %d remains at the same position, which is %d", player,position[player-1]));
+				break;
+			case 2:
+				position[player-1] += diceRoll;
+				System.out.println(String.format("Wow!! it's Ladder, Player %d moves ahead by %d steps", player, diceRoll));
+				System.out.println(String.format("Updated position of the player %d is %d", player, position[player-1]));
+				break;
+			default:
+				position[player-1] -= diceRoll;
+				System.out.println(String.format("Oops.. it's Snake, Player %d moves behind by %d steps", player, diceRoll));
+				System.out.println(String.format("Updated position of the player %d is %d", player, position[player-1]));
+			}
 		}
 
 	}
