6824d9ae812237ebee0a9ada61406ca677e92b3e
ibalram <balramsinghrathore3@gmail.com>
2020-09-25 02:43:02 +0530

    Created: dice roll

--- src/com/cg/SnakeAndLadder.java
+++ src/com/cg/SnakeAndLadder.java
@@ -1,6 +1,7 @@
 package com.cg;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class SnakeAndLadder {
 
@@ -10,8 +11,16 @@ public class SnakeAndLadder {
 		int NO_OF_PLAYERS = 1;
 		int position[] = new int[NO_OF_PLAYERS];
 		Arrays.fill(position, 0);
-		for( int i = 0; i< NO_OF_PLAYERS; ++i) {
-			System.out.println("Position of player "+ String.format("%d",i+1) + " is " + position[i]);
+		
+		int MAX_ROLL = 6;
+		Random rand = new Random();
+		
+		for( int player = 1; player <= NO_OF_PLAYERS; ++player) {
+			System.out.println("Position of player "+ player + " is " + position[player-1]);
+			
+			int diceRoll = rand.nextInt(MAX_ROLL) +1;
+			System.out.println("The dice roll for player "+ player + " is " + diceRoll);
+			
 		}
 
 	}
